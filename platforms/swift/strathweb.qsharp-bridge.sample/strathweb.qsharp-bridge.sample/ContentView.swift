4b7b1155ab4e6cce1e3d0d43bda70471baf5d3a7
filipw <wojcieszyn@gmail.com>
2023-07-17 10:27:26 +0200

    added code view

--- platforms/swift/strathweb.qsharp-bridge.sample/strathweb.qsharp-bridge.sample/ContentView.swift
+++ platforms/swift/strathweb.qsharp-bridge.sample/strathweb.qsharp-bridge.sample/ContentView.swift
@@ -6,40 +6,49 @@
 //
 
 import SwiftUI
+import CodeEditorView
+import LanguageSupport
 
 struct ContentView: View {
-    let code = """
-        namespace MyQuantumApp {
-                @EntryPoint()
-                operation Main() : Unit {
-                    Message("Hello");
-                }
-        }
-    """
+    @State private var code =
+"""
+namespace MyQuantumApp {
+    @EntryPoint()
+        operation Main() : Unit {
+        Message("Hello");
+    }
+}
+"""
+    @State private var position = CodeEditor.Position()
+    @State private var messages: Set<Located<Message>> = Set()
+    @State private var showMinimap = true
+    @State private var wrapText = true
     
     @State var executionState : ExecutionState?
     
     var body: some View {
-       
-        VStack {
-            Text(code).padding()
-            Divider()
-            Button(action: {
-                executionState = try! runQs(source: code)
-            }, label: {
-                Text("Run code!")
-            }).padding()
-            
-            if let result = executionState {
-                ForEach(result.messages, id: \.self) { element in
-                    Text(element)
+        GeometryReader { geometry in
+            VStack {
+                CodeEditor(text: $code, position: $position, messages: $messages, layout: CodeEditor.LayoutConfiguration(showMinimap: showMinimap, wrapText: wrapText)).frame(height: geometry.size.height / 2)
+                
+                Divider()
+                Button(action: {
+                    executionState = try! runQs(source: code)
+                }, label: {
+                    Text("Run code!")
+                }).padding()
+                
+                if let result = executionState {
+                    ForEach(result.messages, id: \.self) { element in
+                        Text(element)
                         
-                }.padding()
-                    .background(Color.yellow)
-                    .cornerRadius(10)
+                    }.padding()
+                        .background(Color.yellow)
+                }
+                Spacer()
             }
+            .padding()
         }
-        .padding()
     }
 }
 
