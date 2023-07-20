ee5516e5febaf29b9532785bfcd7b837dfead505
filipw <wojcieszyn@gmail.com>
2023-07-20 17:00:19 +0200

    further results clean up

--- platforms/swift/strathweb.qsharp-bridge.sample/strathweb.qsharp-bridge.sample/ExecutionResultView.swift
+++ platforms/swift/strathweb.qsharp-bridge.sample/strathweb.qsharp-bridge.sample/ExecutionResultView.swift
@@ -61,36 +61,50 @@ struct SingleShotView: View {
     var executionState: ExecutionState
     
     var body: some View {
-        HStack {
-            
-            VStack(alignment: .leading) {
-                
+        VStack {
+            HStack(alignment: .center) {
                 Label {
-                    Text("Return".uppercased()).font(.caption)
+                    Text("Return:".uppercased()).font(.caption)
                 } icon: {
                     Image(systemName: "doc.badge.gearshape")
                 }
                 
-                Text(toKetIfPossible(input: executionState.result)).font(.title).padding()
-                
                 Spacer()
                 
-                
-            }
-            Divider()
-            VStack {
-                ForEach(executionState.messages, id: \.self) { element in
-                    Text(element).font(.caption)
-                }
+            }.padding(.leading, 75).padding(.top)
+            
+            HStack(alignment: .center) {
+                Text(toKetIfPossible(input: executionState.result)).font(.callout)
                 Spacer()
+            }.padding(.leading, 100).padding(.bottom).padding(.top, 1)
+            
+            if executionState.messages.count > 0 {
+                HStack(alignment: .center) {
+                    Label {
+                        Text("Console output:".uppercased()).font(.caption)
+                    } icon: {
+                        Image(systemName: "terminal")
+                    }
+                    Spacer()
+                }.padding(.leading, 75).padding(.top, 0)
+                
+                HStack(alignment: .center) {
+                    VStack {
+                        ForEach(executionState.messages, id: \.self) { element in
+                            Text(element).font(.caption)
+                        }
+                    }
+                    Spacer()
+                }.padding(.leading, 100).padding(.top, 1)
             }
-        }.padding()
+        }
     }
     
     func toKetIfPossible(input: String?) -> String {
         if var result = input {
-            if result == "Zero" { result = "|0⟩" }
-            if result == "One" { result = "|1⟩" }
+            if result == "Zero" { return "|0⟩" }
+            if result == "One" { return "|1⟩" }
+            if result == "()" { return "None" }
 
             result = result.replacingOccurrences(of: "(", with: "|")
             result = result.replacingOccurrences(of: "[", with: "|")
