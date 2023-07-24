93a955ebc65477523ebce2d37b9314c34cb8187a
filipw <wojcieszyn@gmail.com>
2023-07-24 13:44:53 +0200

    fixed framework build

--- build-ios.sh
+++ build-ios.sh
@@ -4,6 +4,11 @@ OUTDIR="platforms/swift"
 RELDIR="release"
 NAME="qsharp_bridge"
 STATIC_LIB_NAME="lib${NAME}.a"
+NEW_HEADER_DIR="bindings/include"
+
+mkdir -p "${NEW_HEADER_DIR}"
+cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
+cp "bindings/qsharp_bridgeFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
 
 cargo build --target aarch64-apple-darwin --release
 cargo build --target aarch64-apple-ios --release
@@ -13,9 +18,9 @@ rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
 
 xcodebuild -create-xcframework \
     -library "${TARGETDIR}/aarch64-apple-darwin/${RELDIR}/${STATIC_LIB_NAME}" \
-    -headers "${HEADERPATH}" \
+    -headers "${NEW_HEADER_DIR}" \
     -library "${TARGETDIR}/aarch64-apple-ios/${RELDIR}/${STATIC_LIB_NAME}" \
-    -headers "${HEADERPATH}" \
+    -headers "${NEW_HEADER_DIR}" \
     -library "${TARGETDIR}/aarch64-apple-ios-sim/${RELDIR}/${STATIC_LIB_NAME}" \
-    -headers "${HEADERPATH}" \
+    -headers "${NEW_HEADER_DIR}" \
     -output "${OUTDIR}/${NAME}_framework.xcframework"
