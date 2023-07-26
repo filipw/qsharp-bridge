2a0ff61fe8c38694e772d29890aaa31c351ac5fe
filipw <wojcieszyn@gmail.com>
2023-07-26 17:26:04 +0200

    switch up order

--- build-ios.sh
+++ build-ios.sh
@@ -6,13 +6,13 @@ NAME="qsharp_bridge"
 STATIC_LIB_NAME="lib${NAME}.a"
 NEW_HEADER_DIR="bindings/include"
 
-mkdir -p "${NEW_HEADER_DIR}"
-cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
-cp "bindings/qsharp_bridgeFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
-
 cargo build --target aarch64-apple-darwin --release
 cargo build --target aarch64-apple-ios --release
 cargo build --target aarch64-apple-ios-sim --release
+
+mkdir -p "${NEW_HEADER_DIR}"
+cp "${HEADERPATH}" "${NEW_HEADER_DIR}/"
+cp "bindings/qsharp_bridgeFFI.modulemap" "${NEW_HEADER_DIR}/module.modulemap"
 
 rm -rf "${OUTDIR}/${NAME}_framework.xcframework"
 
